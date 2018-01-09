65d4c508de6489c37ec0ca767d8bfaba0c94bfd8
Wesley Barlow <sirwesleybarlow@gmail.com>
2018-01-09 12:48:18 -0700

    Add solution structure

---
+++ BattlecodeBots/BattlecodeBots/main.cpp
@@ -0,0 +1,10 @@
+
+/*
+Each bot should be contained in its own project in this solution
+Code that will be reused between multiple bots (including API) should be contained by this project
+*/
+
+int main(int argc, char** argv)
+{
+
+}
