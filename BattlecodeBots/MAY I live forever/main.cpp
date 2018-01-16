d09efa75cf56b293af0fedff97232de958b97a73
Unknown <evanl1719@gmail.com>
2018-01-16 16:03:03 -0700

    Merged

--- BattlecodeBots/MAY I live forever/main.cpp
+++ BattlecodeBots/MAY I live forever/main.cpp
@@ -159,37 +159,37 @@ int main()
 	//AStar aStar = AStar();
 	//bool pathed = false;
 
-	//if (GameController::Planet() == Mars)
-	//{
-	//	pathed = true;
-	//}
+	if (GameController::Planet() == Mars)
+	{
+		pathed = true;
+	}
 
-	//int pathNum = 0;
+	int pathNum = 0;
 	while (true)
 	{
 		uint32_t round = GameController::Round();
 		std::cout << "Round: " << round << std::endl;
-		//auto units = GameController::Units(bc_Selection::MyTeam);
-		//for (auto unit : units)
-		//{
-		//	if (!pathed)
-		//	{
-		//		int x = unit.Loc().ToMapLocation().X();
-		//		int y = unit.Loc().ToMapLocation().Y();
-		//		aStar.PathFind(x, y, GameMap::Earth().Width() - x, GameMap::Earth().Height() - y, true);
-		//		pathed = true;
-		//	}
-		//	if (unit.type == bc_UnitType::Worker) {
-		//		units::Worker worker{ bc_Unit_clone(unit.self) };
-		//		if (worker.IsMoveReady() && worker.CanMove(aStar.m_pathDirections[pathNum]))
-		//		{
-		//			worker.Move(aStar.m_pathDirections[pathNum]);
-		//			if (pathNum < aStar.m_pathDirections.size())
-		//				pathNum++;
-		//		}
+		auto units = GameController::Units(bc_Selection::MyTeam);
+		for (auto unit : units)
+		{
+			if (!pathed)
+			{
+				int x = unit.Loc().ToMapLocation().X();
+				int y = unit.Loc().ToMapLocation().Y();
+				//aStar.PathFind(x, y, 10, 10, true);
+				pathed = true;
+			}
+			if (unit.type == bc_UnitType::Worker) {
+				units::Worker worker{ bc_Unit_clone(unit.self) };
+				if (worker.IsMoveReady() && worker.CanMove(aStar.m_pathDirections[pathNum]))
+				{
+					worker.Move(aStar.m_pathDirections[pathNum]);
+					if (pathNum < aStar.m_pathDirections.size())
+						pathNum++;
+				}
 
-		//	}
-		//}
+			}
+		}
 		GameController::EndTurn();
 	}
 }
