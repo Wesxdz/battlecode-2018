cbfbcf49c202db41384f2cb37c9e3d7c6efba60a
Unknown <evanl1719@gmail.com>
2018-01-16 17:12:57 -0700

    Commit for Wesley. Playerdata initialization is borken.

--- BattlecodeBots/MAY I live forever/Utility.cpp
+++ BattlecodeBots/MAY I live forever/Utility.cpp
@@ -6,7 +6,7 @@
 
 namespace Utility{
 
-	std::array<int, 5> Utility::tryRotate = { 0, -1, 1, -2, 2 };
+	
 
 	bc_Team GetOtherTeam(bc_Team team) {
 		return team == bc_Team::Red ? bc_Team::Blue : bc_Team::Red;
@@ -20,39 +20,7 @@ namespace Utility{
 		return type < bc_UnitType::Rocket;
 	}
 
-	bool Utility::IsSpotOpen(int x, int y, bool mars)
-	{
-		bool returnBool = false;
-		if (mars)
-		{
-			bc_MapLocation* mp = new_bc_MapLocation(GameMap::Mars().Planet(), x, y);
 
-			returnBool = bc_PlanetMap_is_passable_terrain_at(GameMap::Mars().self, mp);
-
-			if (returnBool)
-			{
-				returnBool = bc_GameController_has_unit_at_location(GameController::gc, mp);
-			}
-
-			delete_bc_MapLocation(mp);
-		}
-		else
-		{
-			bc_MapLocation* mp = new_bc_MapLocation(GameMap::Earth().Planet(), x, y);
-
-			returnBool = bc_PlanetMap_is_passable_terrain_at(GameMap::Earth().self, mp);
-
-			if (returnBool)
-			{
-				returnBool = bc_GameController_has_unit_at_location(GameController::gc, mp);
-			}
-
-			delete_bc_MapLocation(mp);
-		}
-
-
-		return returnBool;
-	}
 
 	//MapLocation GetRandomPosition(GameInfo* info, MapLocation* temp) {
 	//	auto canPassTerrainAt = bc_PlanetMap_is_passable_terrain_at(info->planetMap.self, temp->self);
