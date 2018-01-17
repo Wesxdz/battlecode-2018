2a5430b2a99d3f0e81a4538687bfe95bfc01c448
Unknown <evanl1719@gmail.com>
2018-01-16 17:12:57 -0700

    Commit for Wesley. Playerdata initialization is broken.

--- BattlecodeBots/MAY I live forever/PlayerData.h
+++ BattlecodeBots/MAY I live forever/PlayerData.h
@@ -11,27 +11,37 @@ struct Vec2
 {
 	int x;
 	int y;
+	Vec2(int x2, int y2)
+	{
+		x = x2;
+		y = y2;
+	}
+
 };
 
 class PlayerData
 {
 public:
 	PlayerData();
+	//Updates unit counts and vector of our units.
+	void Update();
 
 	std::map<bc_UnitType, int> teamUnitCounts;
 	std::map<bc_UnitType, int> enemyUnitCounts;
+	void GatherUnitData();
 	void ClearUnitCounts();
 	
 	int earthStartingKarbonite;
-	std::vector<Vec2> initialKarbLocations;
-	std::vector<Vec2> enemySpawnPositions;
+	std::vector<MapLocation> initialKarboniteLocations;
+	std::vector<MapLocation> enemySpawnPositions;
+	std::vector<MapLocation> passableEarthTerrain;
+	std::vector<MapLocation> passableMarsTerrain;
 
+	int optimalLaunchTime;
 	std::vector<int> optimalLaunchRounds;
-
-	std::vector<units::Unit> teamUnits;
 
 	std::map<bc_UnitType, int> enemyResearchBranches;
 
 
 };
-#endif;
+#endif
